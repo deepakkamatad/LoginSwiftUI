99ac014ced038ba40dd3cc048d7dc4252455a092
deepakkamatad <81173954+deepakkamatad@users.noreply.github.com>
2023-09-13 14:05:40 +0530

    Add files via upload

---
+++ LoginSwiftUI/LoginSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  LoginSwiftUI
+//
+//  Created by Deepak Basavaraj Kamatad on 13/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LoginView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginSwiftUI/LoginSwiftUI/LoginSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSwiftUIApp.swift
+//  LoginSwiftUI
+//
+//  Created by Deepak Basavaraj Kamatad on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
